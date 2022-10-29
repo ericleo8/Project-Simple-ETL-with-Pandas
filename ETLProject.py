b82c0b471b9f6de2058f17ba36d5c80011b07b87
ericleo8 <ericleonardo8399@gmail.com>
2022-10-29 11:49:07 +0700

    Project from DQLab

---
+++ ETLProject.py
@@ -0,0 +1,169 @@
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$') #Masukkan regex Anda didalam fungsi extract
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+
+#Masukkan regex Anda didalam fungsi extract
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+
+#Masukkan regex anda pada parameter pertama dari fungsi replace
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'\s+', '')
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'\s+', '')
+
+def func(col):
+    abbrev_name = "%s%s"%(col['first_name'][0],col['last_name'][0]) #Singkatan dari Nama Depan dan Nama Belakang dengan mengambil huruf pertama
+    country = col['country']
+    abbrev_institute = '%s'%(''.join(list(map(lambda word: word[0], col['institute'].split())))) #Singkatan dari value di kolom institute
+    return "%s-%s-%s"%(abbrev_name,country,abbrev_institute)
+
+df_participant['team_name'] = df_participant.apply(func, axis=1)
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'\s+', '')
+
+def func(col):
+    abbrev_name = "%s%s"%(col['first_name'][0],col['last_name'][0])
+    country = col['country']
+    abbrev_institute = '%s'%(''.join(list(map(lambda word: word[0], col['institute'].split()))))
+    return "%s-%s-%s"%(abbrev_name,country,abbrev_institute)
+df_participant['team_name'] = df_participant.apply(func, axis=1)
+
+def func(col):
+    first_name_lower = col['first_name'].lower()
+    last_name_lower = col['last_name'].lower()
+    institute = ''.join(list(map(lambda word: word[0], col['institute'].lower().split()))) #Singkatan dari nama perusahaan dalam lowercase
+
+    if 'Universitas' in col['institute']:
+        if len(col['country'].split()) > 1: #Kondisi untuk mengecek apakah jumlah kata dari country lebih dari 1
+            country = ''.join(list(map(lambda word: word[0], col['country'].lower().split())))
+        else:
+            country = col['country'][:3].lower()
+        return "%s%s@%s.ac.%s"%(first_name_lower,last_name_lower,institute,country)
+
+    return "%s%s@%s.com"%(first_name_lower,last_name_lower,institute)
+
+df_participant['email'] = df_participant.apply(func, axis=1)
+df_participant['email']
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'\s+', '')
+
+def func(col):
+    abbrev_name = "%s%s"%(col['first_name'][0],col['last_name'][0])
+    country = col['country']
+    abbrev_institute = '%s'%(''.join(list(map(lambda word: word[0], col['institute'].split()))))
+    return "%s-%s-%s"%(abbrev_name,country,abbrev_institute)
+df_participant['team_name'] = df_participant.apply(func, axis=1)
+
+def func(col):
+    first_name_lower = col['first_name'].lower()
+    last_name_lower = col['last_name'].lower()
+    institute = ''.join(list(map(lambda word: word[0], col['institute'].lower().split())))
+    
+    if 'Universitas' in col['institute']:
+        if len(col['country'].split()) > 1:
+            country = ''.join(list(map(lambda word: word[0], col['country'].lower().split())))
+        else:
+            country = col['country'][:3].lower()
+        return "%s%s@%s.ac.%s"%(first_name_lower,last_name_lower,institute,country)
+        
+    return "%s%s@%s.com"%(first_name_lower,last_name_lower,institute)
+    
+df_participant['email'] = df_participant.apply(func, axis=1)
+
+
+df_participant['birth_date'] = pd.to_datetime(df_participant['birth_date'], format='%d %b %Y')
+
+import pandas as pd
+df_participant = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/dqthon-participants.csv')
+df_participant['postal_code'] = df_participant['address'].str.extract(r'(\d+)$')
+df_participant['city'] = df_participant['address'].str.extract(r'(?<=\n)(\w.+)(?=,)') 
+df_participant['github_profile'] = 'https://github.com/' + df_participant['first_name'].str.lower() + df_participant['last_name'].str.lower()
+df_participant['cleaned_phone_number'] = df_participant['phone_number'].str.replace(r'^(\+62|62)', '0')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'[()-]', '')
+df_participant['cleaned_phone_number'] = df_participant['cleaned_phone_number'].str.replace(r'\s+', '')
+
+def func(col):
+    abbrev_name = "%s%s"%(col['first_name'][0],col['last_name'][0])
+    country = col['country']
+    abbrev_institute = '%s'%(''.join(list(map(lambda word: word[0], col['institute'].split()))))
+    return "%s-%s-%s"%(abbrev_name,country,abbrev_institute)
+df_participant['team_name'] = df_participant.apply(func, axis=1)
+
+def func(col):
+    first_name_lower = col['first_name'].lower()
+    last_name_lower = col['last_name'].lower()
+    institute = ''.join(list(map(lambda word: word[0], col['institute'].lower().split())))
+    
+    if 'Universitas' in col['institute']:
+        if len(col['country'].split()) > 1:
+            country = ''.join(list(map(lambda word: word[0], col['country'].lower().split())))
+        else:
+            country = col['country'][:3].lower()
+        return "%s%s@%s.ac.%s"%(first_name_lower,last_name_lower,institute,country)
+        
+    return "%s%s@%s.com"%(first_name_lower,last_name_lower,institute)
+    
+df_participant['email'] = df_participant.apply(func, axis=1)
+df_participant['birth_date'] = pd.to_datetime(df_participant['birth_date'], format='%d %b %Y')
+
+
+# Selain punya aturan mengenai format DATE, MySQL juga memberi aturan pada data yang bertipe DATETIME yaitu YYYY-MM-DD HH:mm:ss dengan keterangan:
+
+# YYYY: 4 digit yang menandakan tahun
+# MM: 2 digit yang menandakan bulan
+# DD: 2 digit yang menandakan tanggal
+# HH: 2 digit yang menandakan jam
+# mm: 2 digit yang menandakan menit
+# ss: 2 digit yang menandakan detik
+# Contohnya yaitu: 2021-04-07 15:10:55
+
+# Karena data kita mengenai waktu registrasi peserta (register_time) belum sesuai format yang seharusnya.
+
+# Maka dari itu, tugas Anda yaitu untuk merubah register_time ke format DATETIME sesuai dengan aturan dari MySQL.
+
+# Simpanlah hasil tersebut ke kolom register_at.
+df_participant['register_at'] = pd.to_datetime(df_participant['register_time'], unit='s')
+
